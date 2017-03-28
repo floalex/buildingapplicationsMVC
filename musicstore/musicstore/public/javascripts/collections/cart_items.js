c00ec8f2b11a938e68283ab94c82db99588dd1ba
Flo <flo.liang@my.com>
2017-03-28 00:32:47 +0000

    Adding cart part 3 -- delete item

--- musicstore/musicstore/public/javascripts/collections/cart_items.js
+++ musicstore/musicstore/public/javascripts/collections/cart_items.js
@@ -3,20 +3,39 @@ var CartItems = Backbone.Collection.extend({
     this.total = this.toJSON().reduce(function(a, b) {
       return a + b.price * b.quantity;
     }, 0);
+    
+    return this;
   },
   getTotal: function() { return this.total; },
   setQuantity: function() {
     this.quantity = this.toJSON().reduce(function(a, b) {
       return a + b.quantity;
     }, 0);
+    
+    return this;
   },
   getQuantity: function() { return this.quantity; },
   addItem: function(item) {
-    item = item.clone();
-    item.set("quantity", 1);
-    this.add(item);
-    this.setTotal();
-    this.setQuantity();
+    // check if the item already existed, get the item if the id existed
+    var existing = this.get(item.get("id"));
+    
+    if (existing) {
+      existing.set("quantity", existing.get("quantity") + 1);
+    }
+    else {
+      existing = item.clone();
+      existing.set("quantity", 1);
+      this.add(existing);
+    }
+    this.setTotal().setQuantity();
     this.trigger("cart_updated");
+  },
+  destroy: function(id) {
+    // don't call remove on collection as we need to update total and quantity
+    this.remove(id);
+    this.setTotal().setQuantity();
+  },
+  initialize: function() {
+    this.on("destroy", this.destroy);
   }
 });
--- musicstore/musicstore/public/javascripts/views/cart.js
+++ musicstore/musicstore/public/javascripts/views/cart.js
@@ -1,6 +1,17 @@
 var CartView = Backbone.View.extend({
   template: App.templates.cart,
   el: $("#cart"),
+  events: {
+    "click a.delete": "destroy"
+  },
+  destroy: function(e) {
+    e.preventDefault();
+    var $e = $(e.target);
+    console.log($e);
+    // trigger event that bound to collection
+    this.collection.trigger("destroy", Number($e.attr("data-id")));
+    this.render();
+  },
   render: function() {
     this.$el.html(this.template({
       quantity: this.collection.getQuantity(),
